66c80210795a619a469265d37d374159b17785ea
Diya910 <khetarpaldiya@gmail.com>
2024-02-18 07:06:09 +0530

    second pattern

---
+++ patterns/second.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void print1(int x){
+   for (int i = 0; i < x; i++)
+   {
+    for (int j = 0; j <= i; j++)
+    {
+        cout<<"* ";
+    }
+       cout<<endl;
+   }
+   
+}
+
+int main(){
+    int x; 
+    cin >> x;
+    print1(x);
+    return 0;
+}
