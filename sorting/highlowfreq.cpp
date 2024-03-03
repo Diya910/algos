2ecc577ead900020f050ffe8894e38b68e6ed84f
Diya910 <khetarpaldiya@gmail.com>
2024-03-03 07:22:10 +0530

    High and Low Frequency

---
+++ sorting/highlowfreq.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void countFreq(int arr[],int n)
+{
+   vector<bool> visited(n,false);
+   int maxFreq = 0, minFreq = n;
+   int maxEle = 0, minEle = 0;
+
+   for (int i = 0; i < n; i++){
+
+    if (visited[i] == true)
+    continue;
+
+    int count=1;
+    for(int j = 1+i ; j< n; j++){
+        if (arr[i] == arr[j]) {
+            visited[j] = true;
+            count++;
+        }
+    }
+       if(count>maxFreq){
+        maxFreq = count;
+        maxEle = arr[i];
+       }
+
+       if (count< minFreq){
+        minFreq = count;
+        minEle = arr[i];
+       }
+
+   }
+   cout << "The highest frequency element is: " << maxEle <<"and freq is "<< maxFreq << "\n";
+    cout << "The lowest frequency element is: " << minEle <<"and freq is "<< minFreq << "\n";
+}
+
+int main(){
+   int arr[] = {10, 5, 10, 15, 10, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    countFreq(arr, n);
+    return 0;
+}
